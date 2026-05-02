ce08dac6dbaa39118e60028b409c8081dfa7352c
miguelangelmz21 <mike21.mzeb@gmail.com>
2026-05-02 00:01:03 -0500

    feat: work. Add ProductsComponent with product listing and styling; refactor ImgComponent and AppComponent

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -9,34 +9,7 @@ export class AppComponent {
   // imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
   imgParent = '';
   showImg = true;
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-  ];
-
-  
+    
   onLoaded(img:string) {
     console.log('log padre', img);
   }
