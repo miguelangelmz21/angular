46f5577c69a4b6c21d37584e6d5ec8e27c43f13f
miguelangelmz21 <mike21.mzeb@gmail.com>
2026-05-06 22:41:11 -0500

    feat: Implement responsive navigation menu with toggle functionality. Sidebar navigation with hamburger menu toggle for mobile devices.

--- my-store/src/app/components/nav/nav.component.ts
+++ my-store/src/app/components/nav/nav.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  
+  activeMenu = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
   }
 
 }
